a290e4e1f8f15efd76f3af5887d4171d014ce85e
wangnz <5323700@qq.com>
2017-11-16 15:47:39 +0800

    upd

--- mytest/src/helloworld/HelloWorld.java
+++ mytest/src/helloworld/HelloWorld.java
@@ -4,8 +4,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("hello world1");
+		System.out.println("master");
 	}
 
 }
